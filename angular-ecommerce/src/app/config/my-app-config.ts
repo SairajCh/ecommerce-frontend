a7a352e25ec8c3ce610782f84bfd24428b2fa070
SairajCh <sairajchanagani30@gmail.com>
2024-02-25 18:05:08 +0530

    commit just after spring_security

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default  {
+
+    oidc: {
+        clientId: '0oaedn9nxwjZmLzmo5d7',
+        issuer: 'https://dev-63518763.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile','email']
+    }
+
+
+}
--- angular-ecommerce/src/app/services/cart.service.ts
+++ angular-ecommerce/src/app/services/cart.service.ts
@@ -1,7 +1,7 @@
 import { Product } from './../common/product';
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +10,9 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
-
+  totalPrice: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  
   constructor() { }
 
   addToCart(theCartItem: CartItem) {
